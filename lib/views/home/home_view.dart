ee71d581462ebfff31926b34dbf2333451b10907
ae-allison <ansonervin@yahoo.com>
2021-03-04 22:50:35 -0600

    Created a drawer and mobile nav

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -9,11 +9,69 @@ import 'package:timeless_app/widgets/home/landing_page_content.dart';
 import 'package:timeless_app/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key key}) : super(key: key);
+  HomeView({Key key}) : super(key: key);
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  void _openDrawer() {
+    _scaffoldKey.currentState.openDrawer();
+  }
+
+  void _closeDrawer() {
+    _scaffoldKey.currentState.openDrawer();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomTextLarge(
+                        text: 'AnsonErvin Inc.', color: Colors.white)
+                  ]),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            ListTile(
+              title: CustomTextNormal(text: 'Home'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: CustomTextNormal(text: 'About'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: CustomTextNormal(text: 'Search'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: Text('Contact'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ), // Populate the Drawer in the next step.
+      ),
       appBar: !kIsWeb
           ? AppBar(
               title: CustomTextNormal(
@@ -31,7 +89,12 @@ class HomeView extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            kIsWeb ? NavigationBar() : Container(),
+            kIsWeb
+                ? NavigationBar(
+                    openDrawer: _openDrawer,
+                    closeDrawer: _closeDrawer,
+                  )
+                : Container(),
             LandingPageContent(),
             kIsWeb ? Container() : CustomTabBar()
           ]),
--- lib/widgets/navigation_bar/navigation_bar.dart
+++ lib/widgets/navigation_bar/navigation_bar.dart
@@ -1,13 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:timeless_app/utils/custom_section.dart';
 import 'package:timeless_app/widgets/navigation_bar/nav_item.dart';
 
 class NavigationBar extends StatelessWidget {
-  const NavigationBar({Key key}) : super(key: key);
+  final Function openDrawer;
+  final Function closeDrawer;
+
+  const NavigationBar({this.openDrawer, this.closeDrawer});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool showMobileNav = screenWidth < 600 && kIsWeb;
+
     return CustomSection(
       color: Colors.black87,
       child: Container(
@@ -21,15 +28,24 @@ class NavigationBar extends StatelessWidget {
               width: 50,
               child: Image.asset('assets/images/logo.png'),
             ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const NavBarItem('Home'),
-                const NavBarItem('Search'),
-                const NavBarItem('About'),
-                const NavBarItem('Contact'),
-              ],
-            )
+            !showMobileNav
+                ? Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const NavBarItem('Home'),
+                      const NavBarItem('Search'),
+                      const NavBarItem('About'),
+                      const NavBarItem('Contact'),
+                    ],
+                  )
+                : IconButton(
+                    onPressed: () {
+                      openDrawer();
+                    },
+                    icon: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ))
           ],
         ),
       ),
