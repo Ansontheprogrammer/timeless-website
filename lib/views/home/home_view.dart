0089990906cee99641e64a99cbc94026eb1d1c90
ae-allison <ansonervin@yahoo.com>
2021-03-07 14:40:39 -0600

    Deleted app bar file and brought it back to home_view

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -5,12 +5,11 @@ import 'package:timeless_app/utils/custom_text.dart';
 import 'package:timeless_app/widgets/navigation/app/custom_tab_bar.dart';
 import 'package:timeless_app/widgets/home/landing_page_content.dart';
 import 'package:timeless_app/widgets/navigation/drawer/drawer.dart';
-import 'package:timeless_app/widgets/navigation/drawer/drawer_header.dart';
 import 'package:timeless_app/widgets/navigation/nav-bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   HomeView();
-  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   void _openDrawer() {
     _scaffoldKey.currentState!.openDrawer();
@@ -23,7 +22,18 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: !kIsWeb
+          ? AppBar(
+              title: CustomTextNormal(
+                  text: 'Timeless on an App', color: Colors.white),
+              backgroundColor: Colors.black87,
+            )
+          : PreferredSize(
+              child: Container(),
+              preferredSize: Size(
+                0,
+                0,
+              )),
       key: _scaffoldKey,
       drawer: CustomDrawer(),
       backgroundColor: Colors.white,
