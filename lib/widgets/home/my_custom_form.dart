508223375a7905c9bf24a4130adfcf7be1528820
ae-allison <ansonervin@yahoo.com>
2021-03-11 00:55:51 -0600

    Improved signup form container

--- lib/widgets/home/my_custom_form.dart
+++ lib/widgets/home/my_custom_form.dart
@@ -1,7 +1,9 @@
 // Create a Form widget.
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:timeless_app/utils/container_spacer.dart';
 import 'package:timeless_app/utils/custom_text.dart';
+import 'package:timeless_app/utils/small_spacer.dart';
 
 class MyCustomForm extends StatefulWidget {
   @override
@@ -11,11 +13,6 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
   String? Function(String? value) Function({required String title}) validator =
       ({required String title}) {
@@ -27,53 +24,98 @@ class MyCustomFormState extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return Container(
-      width: 100.0,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-              child: Center(
-            child: CustomTextNormal(
-              fontWeight: FontWeight.bold,
-              text: 'Want your business added?',
-            ),
-          )),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Name'),
-            validator: validator(title: 'Name'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Center(
+          child: CustomTextHeadline(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            text: 'Want your business added?',
           ),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Business'),
-            validator: validator(title: 'Business'),
-          ),
-          TextFormField(
-              decoration: InputDecoration(hintText: 'Location'),
-              validator: validator(title: 'Location')),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Website'),
-            validator: validator(title: 'Website'),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Submitting Data Now')));
-                }
-              },
-              child: CustomTextBtn(
-                color: Colors.white,
-                text: 'Submit',
+        ),
+        ContainerSpacer(),
+        TextFormField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Name',
+            border: new OutlineInputBorder(
+              borderRadius: const BorderRadius.all(
+                const Radius.circular(2.5),
               ),
             ),
           ),
-        ],
-      ),
+          validator: validator(
+            title: 'Name',
+          ),
+        ),
+        SmallSpacer(),
+        TextFormField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Business',
+            border: new OutlineInputBorder(
+              borderRadius: const BorderRadius.all(
+                const Radius.circular(2.5),
+              ),
+            ),
+          ),
+          validator: validator(title: 'Business'),
+        ),
+        SmallSpacer(),
+        TextFormField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Location',
+              border: new OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(2.5),
+                ),
+              ),
+            ),
+            validator: validator(title: 'Location')),
+        SmallSpacer(),
+        TextFormField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Website',
+            border: new OutlineInputBorder(
+              borderRadius: const BorderRadius.all(
+                const Radius.circular(2.5),
+              ),
+            ),
+          ),
+          validator: validator(title: 'Website'),
+        ),
+        Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.25,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false
+                  // otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a Snackbar.
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Submitting Data Now')));
+                  }
+                },
+                child: CustomTextBtn(
+                  color: Colors.white,
+                  text: 'Submit',
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
