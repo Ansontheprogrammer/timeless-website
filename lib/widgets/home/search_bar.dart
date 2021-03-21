96c534a91e4c983b8748e3cf257a1fd72ba6a531
ae-allison <ansonervin@yahoo.com>
2021-03-21 07:38:02 -0500

    Improved search bar ui

--- lib/widgets/home/search_bar.dart
+++ lib/widgets/home/search_bar.dart
@@ -38,65 +38,68 @@ class _SearchBarState extends State<SearchBar> with TickerProviderStateMixin {
     return FadeTransition(
       opacity: animation,
       child: Material(
-          child: !loading
-              ? TextField(
-                  textInputAction: TextInputAction.done,
-                  onSubmitted: (String text) async {
-                    setState(() {
-                      loading = true;
-                    });
-                    Map<String, dynamic> _params = {
-                      'key': DotEnv.env['API_KEY'],
-                      'cx': DotEnv.env['SEARCH_ENGINE_ID'],
-                      'q': text
-                    };
-                    Uri url = Uri.https(_authority, _path, _params);
+        color: Colors.transparent,
+        child: !loading
+            ? TextField(
+                textInputAction: TextInputAction.done,
+                onSubmitted: (String text) async {
+                  setState(() {
+                    loading = true;
+                  });
+                  Map<String, dynamic> _params = {
+                    'key': DotEnv.env['API_KEY'],
+                    'cx': DotEnv.env['SEARCH_ENGINE_ID'],
+                    'q': text
+                  };
+                  Uri url = Uri.https(_authority, _path, _params);
 
-                    SnackBar errorSnackBar = SnackBar(
-                        content: Text(
-                            'Sorry we were not able to complete your search'));
-                    try {
-                      http.Response response = await http.get(
-                        url,
-                      );
-                      if (response.statusCode < 300 &&
-                          response.statusCode >= 200) {
-                        SnackBar snackBar =
-                            SnackBar(content: Text('Search successful'));
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        setState(() {
-                          searchResult = response.body;
-                        });
-                        print({'search', jsonDecode(response.body)});
-                      } else if (response.statusCode == 200) {
-                        ScaffoldMessenger.of(context)
-                            .showSnackBar(errorSnackBar);
-                      }
-                    } catch (err) {
+                  SnackBar errorSnackBar = SnackBar(
+                      content: Text(
+                          'Sorry we were not able to complete your search'));
+                  try {
+                    http.Response response = await http.get(
+                      url,
+                    );
+                    if (response.statusCode < 300 &&
+                        response.statusCode >= 200) {
+                      SnackBar snackBar =
+                          SnackBar(content: Text('Search successful'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      setState(() {
+                        searchResult = response.body;
+                      });
+                      print({'search', jsonDecode(response.body)});
+                    } else if (response.statusCode == 200) {
                       ScaffoldMessenger.of(context).showSnackBar(errorSnackBar);
                     }
-                    setState(() {
-                      loading = false;
-                    });
-                  },
-                  onChanged: (text) {
-                    print("First text field: $text");
-                  },
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      suffixIcon: Icon(Icons.search),
-                      border: new UnderlineInputBorder(
-                          borderRadius: const BorderRadius.all(
-                            const Radius.circular(50.0),
-                          ),
-                          borderSide: new BorderSide(color: Colors.red)),
-                      hintText: searchResult == ''
-                          ? 'Enter a search term'
-                          : searchResult),
-                )
-              : Container(
-                  height: 50, width: 50, child: CircularProgressIndicator())),
+                  } catch (err) {
+                    ScaffoldMessenger.of(context).showSnackBar(errorSnackBar);
+                  }
+                  setState(() {
+                    loading = false;
+                  });
+                },
+                onChanged: (text) {
+                  print("First text field: $text");
+                },
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 20.0, right: 20.0),
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: Icon(Icons.business),
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(25.0),
+                      ),
+                    ),
+                    hintText: searchResult == ''
+                        ? 'Enter a search term'
+                        : searchResult),
+              )
+            : Container(
+                height: 50, width: 50, child: CircularProgressIndicator()),
+      ),
     );
   }
 }
