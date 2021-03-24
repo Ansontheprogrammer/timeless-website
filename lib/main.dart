8bdf35fe90ba26cda145a410f58684d750a455f0
ae-allison <ansonervin@yahoo.com>
2021-03-23 21:42:40 -0500

    Added ignore for google-services.json Integrated firebase into project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:timeless_app/views/home/home_view.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+
+import 'utils/custom_text.dart';
 
 void main() async {
   await DotEnv.load(fileName: ".env");
@@ -9,7 +12,39 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Initialize firebase app
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Error retrieving Firebase
+          if (snapshot.hasError) {
+            return Container(
+              child: CustomTextNormal(
+                text: "There was an error accessing our app",
+              ),
+            );
+          }
+
+          // Firebase retrieved
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MainApp();
+          }
+
+          // Waiting for Firebase to load
+          return CircularProgressIndicator();
+        });
+  }
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
