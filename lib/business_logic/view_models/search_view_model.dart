0ef871cec107339cbcf463b158c97cab0f0a3351
ae-allison <ansonervin@yahoo.com>
2021-04-23 08:36:43 -0500

    Created save search provider Removed save search functionality from view model

--- lib/business_logic/view_models/search_view_model.dart
+++ lib/business_logic/view_models/search_view_model.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:timeless_app/business_logic/enums/business_search.dart';
 import 'package:timeless_app/business_logic/models/query.dart';
+import 'package:timeless_app/business_logic/providers/save_search_provider.dart';
+import 'package:timeless_app/services/locator.dart';
 
 /// Used for providing query searches in the search view
 class SearchViewModel extends ChangeNotifier {
   QuerySearch _activeSearch =
       QuerySearch(fieldName: BusinessSearchTypes.Name, search: '');
 
+  SaveSearchProvider saveSearchProvider = locator<SaveSearchProvider>();
   QuerySearch get activeSearch {
     return _activeSearch;
-  }
-
-  List<QuerySearch> _recentSearches = [];
-  List<QuerySearch> get recentSearches {
-    return _recentSearches;
   }
 
   String _nameSearch = '';
@@ -24,26 +22,6 @@ class SearchViewModel extends ChangeNotifier {
   String _locationSearch = '';
   String get locationSearch {
     return _locationSearch;
-  }
-
-  saveSearch(BusinessSearchTypes type) {
-    print({nameSearch, 'type', type, 'saving search'});
-    if (type == BusinessSearchTypes.Name) {
-      _recentSearches.add(
-          QuerySearch(fieldName: BusinessSearchTypes.Name, search: nameSearch));
-      _nameSearch = '';
-    } else {
-      _recentSearches.add(QuerySearch(
-          fieldName: BusinessSearchTypes.Zipcode, search: locationSearch));
-      _locationSearch = '';
-    }
-
-    notifyListeners();
-  }
-
-  dynamic removeSearch(QuerySearch search) {
-    _recentSearches.remove(search);
-    notifyListeners();
   }
 
   void changeSearch(QuerySearch searchQuery) {
@@ -59,6 +37,14 @@ class SearchViewModel extends ChangeNotifier {
         break;
     }
     notifyListeners();
+  }
+
+  void clearSearch(BusinessSearchTypes type) {
+    if (type == BusinessSearchTypes.Name) {
+      _nameSearch = '';
+    } else {
+      _locationSearch = '';
+    }
   }
 
   changeActiveSearch(QuerySearch newSearch) {
