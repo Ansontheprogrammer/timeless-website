1af975e7e6887265568d972334b98a24cb7910e6
ae-allison <ansonervin@yahoo.com>
2021-04-24 10:27:25 -0500

    Added parsed JSON to the abstract FirestoreDoc

--- lib/business_logic/utils/firestore_doc.dart
+++ lib/business_logic/utils/firestore_doc.dart
@@ -1,5 +1,13 @@
-abstract class FirestoreDoc {
+/// Creates an abstract class that will ensure models that need to be stored in the DB implement required methods and have required properties
+///
+/// Models need to have an 'id' that is a String
+abstract class FirestoreDoc<T> {
+  /// Unique identifier for document.
   abstract String id;
 
+  /// Converts a model to JSON format to prepare for storage.
   Map<String, dynamic> toJSON();
+
+  /// Parses from JSON into the respective model, when pulling the data in the app.
+  FirestoreDoc.fromJSON(Map<String, dynamic> parsedJSON);
 }
