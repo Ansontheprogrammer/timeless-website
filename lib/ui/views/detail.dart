1d53f356aa24c9f1f907c997829f8032ef22b2cc
ae-allison <ansonervin@yahoo.com>
2021-05-26 12:07:31 -0500

    Create web design for detail page Create adaptive widget

--- lib/ui/views/detail.dart
+++ lib/ui/views/detail.dart
@@ -4,7 +4,7 @@ import 'package:timeless_app/business_logic/models/business.dart';
 import 'package:timeless_app/ui/shared/utils/custom_text.dart';
 import 'package:timeless_app/ui/shared/detail_screen_arguments.dart';
 import 'package:timeless_app/ui/shared/utils/layout.dart';
-import 'package:timeless_app/ui/views/contact.dart';
+import 'package:timeless_app/ui/widgets/detail/adaptive_detail_item.dart';
 
 import '../shared/constants.dart';
 
@@ -19,45 +19,27 @@ class DetailView extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as DetailScreenArguments;
     Business business = args.business;
     return Layout(
-        pageContent: Padding(
-      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Hero(
-                tag: Constants.getBusinessHeroImage(business.id),
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage(business.imageURL ??
-                      'https://images.unsplash.com/photo-1613664161831-35ca95a4b953?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80'),
-                ),
-              ),
-              SizedBox(width: 25),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Hero(
-                        tag: Constants.getBusinessHeroTitle(business.id),
-                        child: CustomTextNormal(
-                          fontWeight: FontWeight.bold,
-                          text: business.getBusinessName(),
-                        )),
-                    Hero(
-                      tag: Constants.getBusinessHeroTitle(business.description),
-                      child: Container(
-                        child: CustomTextNormal(
-                          text: business.description,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+        pageContent: Container(
+      padding: const EdgeInsets.all(25.0),
+      child: AdaptiveDetailItem(
+        business: business,
+        imageTag: Constants.getBusinessHeroImage(business.id),
+        imageUrl: business.imageURL ??
+            'https://images.unsplash.com/photo-1613664161831-35ca95a4b953?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80',
+        name: Hero(
+            tag: Constants.getBusinessHeroTitle(business.id),
+            child: CustomTextNormal(
+              fontWeight: FontWeight.bold,
+              text: business.getBusinessName(),
+            )),
+        description: Hero(
+          tag: Constants.getBusinessHeroTitle(business.description),
+          child: Container(
+            child: CustomTextNormal(
+              text: business.description,
+            ),
           ),
-        ],
+        ),
       ),
     ));
   }
---
+++ lib/ui/widgets/detail/adaptive_detail_item.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:timeless_app/business_logic/models/business.dart';
+import 'package:timeless_app/ui/shared/utils/custom_text.dart';
+
+class AdaptiveDetailItem extends StatelessWidget {
+  const AdaptiveDetailItem(
+      {Key? key,
+      required this.imageTag,
+      required this.imageUrl,
+      required this.name,
+      required this.description,
+      required this.business})
+      : super(key: key);
+
+  final String imageTag;
+  final String imageUrl;
+  final Hero name;
+  final Hero description;
+  final Business business;
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      return Container(
+        color: Colors.black12,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Hero(
+              tag: imageTag,
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: NetworkImage(imageUrl))),
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.6,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.black12,
+                constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height * 0.6),
+                child: Stack(
+                  children: [
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        constraints: BoxConstraints(
+                            minHeight:
+                                MediaQuery.of(context).size.height * 0.6),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.only(
+                                      top: MediaQuery.of(context).size.height *
+                                          0.05),
+                                  child: name),
+                              Container(
+                                  margin: EdgeInsets.only(
+                                      top: MediaQuery.of(context).size.height *
+                                          0.1),
+                                  child: description),
+                            ])),
+                    Positioned(
+                      bottom: 10,
+                      left: 20,
+                      child: Row(
+                        children: [
+                          Icon(Icons.location_pin),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          CustomTextCaption(text: business.zipcode)
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 10,
+                        right: 20,
+                        child: CustomTextCaption(
+                          text: business.website,
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Hero(
+            tag: imageTag,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage(imageUrl))),
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.5,
+            ),
+          ),
+          SizedBox(width: 25),
+          name,
+          description
+        ],
+      );
+    }
+  }
+}
