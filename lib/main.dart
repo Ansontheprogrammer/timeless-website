d378a1907ed53dd390465461490642226fe4eb5d
Anson Ervin <ansonervin@yahoo.com>
2021-06-07 02:00:01 -0500

    Fixed app name

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Timeless App',
+      title: 'Timeless',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           appBarTheme: AppBarTheme(
